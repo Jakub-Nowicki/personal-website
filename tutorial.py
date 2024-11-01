704116605439c9bcfc0938c89b25558b60de7f42
Jakub-Nowicki <148172103+Jakub-Nowicki@users.noreply.github.com>
2024-11-01 14:54:10 -0500

    update

--- tutorial.py
+++ tutorial.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'hello world'
+def home_page():
+    return render_template('home.hmtl')
