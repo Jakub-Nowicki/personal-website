9db1c5fd23416df6fb87477bbeee09f2c295f740
Jakub-Nowicki <148172103+Jakub-Nowicki@users.noreply.github.com>
2024-11-08 14:47:27 -0600

    update

--- portfolio.py
+++ portfolio.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
-@app.route('/projects')
+@app.route('/about')
 def about_page():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects_page():
     return render_template('projects.html')
+
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
