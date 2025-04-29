4873c11815edbdd2790b246566b896d10b66cae6
Jakub Nowicki <148172103+Jakub-Nowicki@users.noreply.github.com>
2025-04-28 22:29:42 -0500

    update

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,9 @@ def create_app():
     from .routes.home import home_bp
     from .routes.sudoku import sudoku_bp
     from .routes.converter import converter_bp
-    from .routes.github_api import github_bp
 
     app.register_blueprint(home_bp)
     app.register_blueprint(sudoku_bp)
     app.register_blueprint(converter_bp)
-    app.register_blueprint(github_bp)
 
     return app
